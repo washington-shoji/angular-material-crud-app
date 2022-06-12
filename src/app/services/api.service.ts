161ce83d3f3f67436b1d284774c20193ef5e216c
washington-shoji <shoji.washington@gmail.com>
2022-06-12 10:55:15 +1000

    feat(service): Added api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  postProduct(data: any) {
+    return this.http.post<any>('http://localhost:8000/productList/', data);
+  }
+
+  getProduct() {
+    return this.http.get<any>('http://localhost:8000/productList/');
+  }
+
+  updateProduct(data: any, id: number) {
+    return this.http.put<any>('http://localhost:8000/productList/' + id, data);
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete<any>('http://localhost:8000/productList/' + id);
+  }
+}
